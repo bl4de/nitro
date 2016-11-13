0377e4b1e6194b6c6dee0e587c5392cac7e33c8c
bloorq@gmail.com <bloorq@gmail.com>
2016-11-13 00:39:25 +0000

    Basic tweets parsing

--- site/app.js
+++ site/app.js
@@ -1,15 +1,43 @@
 'use strict'
 
 const CONTENT = document.getElementById('content')
+const DEFAULT_USERNAME = '@NitroHQ'
 
-function appendTwit(twit) {
-    let twit_content = document.createElement('div')
-    twit_content.textContent = `user: ${twit.user.screen_name} - ${twit.text}`
-    CONTENT.appendChild(twit_content)
+/**
+ * Creates single tweet element and appends to content 
+ * 
+ * @param {object} tweet
+ */
+function appendTwit(tweet) {
+    let tweet_content = document.createElement('div')
+    tweet_content.classList = 'tweet-content'
+    tweet_content.textContent = `user: ${tweet.user.screen_name} - ${tweet.text}`
+    CONTENT.appendChild(tweet_content)
 }
 
-fetch('../dev/sample.json').then(result => {
-    result.json().then(tweets => {
-        tweets.statuses.forEach(appendTwit)
+/**
+ * Process every tweet in returned array of user's tweets
+ * 
+ * @param {any} [tweets=[]]
+ */
+function parseTweets(tweets = []) {
+    if (tweets.length > 0) {
+        tweets.forEach(appendTwit)
+    }
+}
+
+/**
+ * Fetching tweets posted by username
+ * 
+ * @param {string} [username=DEFAULT_USERNAME]
+ */
+function fetchTweets(username = DEFAULT_USERNAME) {
+    //tbd
+    fetch('../dev/sample.json').then(result => {
+        result.json().then(tweets => parseTweets(tweets.statuses))
     })
-})
+}
+
+
+// run app
+fetchTweets()
